4dc73cc29ef9ea06fbe58af440e7eb607dc24a91
luizosb <luiz-osb@hotmail.com>
2025-05-14 11:00:04 -0300

    Paginacao default na lista de medicos e adicao de log do SQL no Spring

--- src/main/java/med/voll/api/medico/controller/MedicoController.java
+++ src/main/java/med/voll/api/medico/controller/MedicoController.java
@@ -7,6 +7,7 @@ import med.voll.api.medico.service.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,9 +36,12 @@ public class MedicoController {
      *Para buscar paginação com essa rota basta utilizar assim no postman:
      * http://localhost:8080/medicos?size=1&page=1 onde size é a quantidade de dados que irá trazer e page a pagina.
      * São parametros que existem no spring para facilitar o processo.
+     *
+     * No entanto, utilizamos a anotação PageableDefault para setar a quantidade, paginação e em qual ordem irá trazer os dados,
+     * sem necessidade de escrever no Postman. Utilizo o postman apenas caso queira editar minha pesquisa.
      */
     @GetMapping
-    public Page<MedicoListagemDTO> listar(Pageable paginacao){
+    public Page<MedicoListagemDTO> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
         return medVollService.buscarTodosMedicos(paginacao);
     }
 }
