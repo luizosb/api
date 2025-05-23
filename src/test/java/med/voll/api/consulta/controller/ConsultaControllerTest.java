2c25e34c59c32720eee946bb7f3d2f90de673422
luizosb <luiz-osb@hotmail.com>
2025-05-23 13:06:36 -0300

    Teste automatizado para controller de consulta

--- src/test/java/med/voll/api/consulta/controller/ConsultaControllerTest.java
+++ src/test/java/med/voll/api/consulta/controller/ConsultaControllerTest.java
@@ -1,25 +1,45 @@
 package med.voll.api.consulta.controller;
 
+import med.voll.api.consulta.DTO.DadosAgendamentoConsulta;
+import med.voll.api.consulta.DTO.DadosDetalhamentoConsulta;
+import med.voll.api.consulta.servico.AgendaDeConsultasService;
+import med.voll.api.medico.model.Especialidade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJSON;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJSON;
+
+    @MockBean
+    private AgendaDeConsultasService agendaDeConsultasService;
 
     @Test
     @DisplayName("Deveria devolver HTTP 400 quando informações estão inválidas")
@@ -28,5 +48,34 @@ class ConsultaControllerTest {
         var response = mvc.perform(post("/consultas")).andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver HTTP 200 quando informações estão válidas")
+    @WithMockUser
+    void agendarCenario2() throws Exception {
+
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2L, 5L, data)
+
+        when(agendaDeConsultasService.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mvc.perform(post("/consultas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosAgendamentoConsultaJSON.write(
+                                new DadosAgendamentoConsulta(2L, 5L, data, especialidade)).getJson()
+                        )
+                )
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoConsultaJSON.write(
+                dadosDetalhamento
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
